681869f7c43e476e95e9863f1884c80530d5baf4
burak_p <burak_p@lwo.by>
2020-09-23 15:51:20 +0300

    charselected.go

--- gameserver/serverpackets/charselected.go
+++ gameserver/serverpackets/charselected.go
@@ -5,53 +5,54 @@ import "l2gogameserver/packets"
 func NewCharSelected() []byte {
 
 	buffer := new(packets.Buffer)
+	//cSdSddddddddddFFdQddddddddddddddddBd
 
-	buffer.WriteSingleByte(0x0b)
-	buffer.WriteS("test")
-	buffer.WriteD(1)
-	buffer.WriteS("hello") //title
-	buffer.WriteD(1)       //sessionId
-	buffer.WriteD(0)       //clanId
-	buffer.WriteD(0)       // ?
-	buffer.WriteD(1)       //sex
-	buffer.WriteD(0)       //race
-	buffer.WriteD(0)       //classId
-	buffer.WriteD(1)       // ?
+	buffer.WriteSingleByte(0x0b) // 1
+	buffer.WriteS("test")        // 11
+	buffer.WriteD(268481286)     // 15
+	buffer.WriteS("")            //title // 21 2 нуля
+	buffer.WriteD(1)             //sessionId //25
+	buffer.WriteD(0)             //clanId // 29
+	buffer.WriteD(0)             // ? //33
+	buffer.WriteD(1)             //sex// 37
+	buffer.WriteD(0)             //race 41
+	buffer.WriteD(0)             //classId 45
+	buffer.WriteD(1)             // ? 49
 
-	buffer.WriteD(82744)  //x
-	buffer.WriteD(148536) //y
-	buffer.WriteD(3400)   //z
+	buffer.WriteD(82744)  //x 53
+	buffer.WriteD(148536) //y 57
+	buffer.WriteD(3400)   //z 61
 
-	buffer.WriteF(444.0)  //currentHP
-	buffer.WriteF(1000.0) //currentMP
+	buffer.WriteF(126.0) //currentHP 69
+	buffer.WriteF(38.0)  //currentMP 77
 
-	buffer.WriteD(0)   // SP
-	buffer.WriteQ(500) // EXP
-	buffer.WriteD(3)   // level
-	buffer.WriteD(0)   // karma
-	buffer.WriteD(1)   // pk
+	buffer.WriteD(0) // SP 81
+	buffer.WriteQ(0) // EXP 89
+	buffer.WriteD(1) // level 93
+	buffer.WriteD(0) // karma 97
+	buffer.WriteD(0) // pk 101
 
-	buffer.WriteD(5) //int
-	buffer.WriteD(5) //str
-	buffer.WriteD(5) //con
-	buffer.WriteD(5) //men
-	buffer.WriteD(5) //dex
-	buffer.WriteD(5) //wit
+	buffer.WriteD(45) //int 105
+	buffer.WriteD(45) //str 109
+	buffer.WriteD(45) //con 113
+	buffer.WriteD(45) //men 117
+	buffer.WriteD(45) //dex 121
+	buffer.WriteD(45) //wit 125
 
-	buffer.WriteD(1) //inGameTime
-	buffer.WriteD(0) //
+	buffer.WriteD(0) //inGameTime 129
+	buffer.WriteD(0) // 133 //tODO
 
-	buffer.WriteD(0)
+	buffer.WriteD(0) // 137
 
-	buffer.WriteD(0) //
-	buffer.WriteD(0) //
-	buffer.WriteD(0) //
-	buffer.WriteD(0) //
+	buffer.WriteD(0) // 141
+	buffer.WriteD(0) // 145
+	buffer.WriteD(0) // 149
+	buffer.WriteD(0) // 153
 
 	m := make([]byte, 64)
-	buffer.WriteSlice(m)
+	buffer.WriteSlice(m) //217
 
-	buffer.WriteD(0)
+	buffer.WriteD(20000) // 221
 
 	return buffer.Bytes()
 
